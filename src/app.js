629b066c3c98277bb8753adb25250669cccd6aba
Vijaykanth-tech <vijayakanth.tech@gmail.com>
2025-08-17 07:31:35 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello, dev!");
-});
-app.use("/hello", (req, res) => {
-  res.send("Hello Helloo, World!");
+app.get("/user", (req, res) => {
+  res.send({
+    name: "John Doe",
+    age: 30,
+    email: "4G2M5@example.com",
+  });
 });
 
+app.post("/user", (req, res) => {
+  res.send({
+    message: "User created successfully",
+  });
+});
+app.delete("/user", (req, res) => {
+  res.send({
+    message: "User deleted successfully",
+  });
+});
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
 });
