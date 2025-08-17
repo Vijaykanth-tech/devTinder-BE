fb15e56a2613af491ae9c13d8408b42c72a5a44a
Vijaykanth-tech <vijayakanth.tech@gmail.com>
2025-08-17 16:07:53 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app = express();
 app.use(express.json()); //It is a Middleware to parse JSON request bodies in Express.js
+
+//signup route to create a new user
 app.post("/signup", async (req, res) => {
   console.log("Received request to create a new user", req.body);
   const userObj = req.body;
@@ -18,6 +20,33 @@ app.post("/signup", async (req, res) => {
       console.error("Error saving user:", error);
       res.status(500).send("Error saving user");
     });
+});
+
+app.get("/getUser", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const user = await User.find({ email: userEmail });
+    console.log("Fetched user:", user);
+    if (!user || user.length === 0) {
+      return res.status(404).send("User not found");
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).send("Error fetching user");
+  }
+});
+
+//get all users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find();
+    console.log("Fetched users:", users);
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).send("Error fetching users");
+  }
 });
 
 connectDB()
